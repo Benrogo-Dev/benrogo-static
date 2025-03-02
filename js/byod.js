98f4edb30669029688790e034529dc75bc274a0b
BenRogozinski <brogodev@gmail.com>
2025-03-01 23:12:13 -0600

    Finalized BYOD script and updated notice on the BYOD page

--- js/byod.js
+++ js/byod.js
@@ -1,6 +1,23 @@
-function registerBYOD() {
+async function registerBYOD(event) {
     const domain = document.getElementById("domain-input").value;
-    console.log(domain);
+    if (domain) {
+        registrationStatusResponse = await fetch("https://dev.benrogo.net/edge-api/registerBYOD", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                domain: domain
+            })
+        });
+        registrationStatusJSON = await registrationStatusResponse.json();
+        if (registrationStatusResponse.ok) {
+            displayNotification(`The domain "${domain}" has been registered successfully!`);
+        } else {
+            displayNotification(`Error: ${registrationStatusJSON.error}`);
+        }
+        console.log(registrationStatusJSON);
+    }
 }
 
-document.getElementById("submit-button").addEventListener("click", registerBYOD());
+document.getElementById("submit-button").addEventListener("click", registerBYOD);
