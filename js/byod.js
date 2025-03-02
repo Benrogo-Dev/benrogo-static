8b955c86fb761947657239168fed0ee91ab99984
BenRogozinski <brogodev@gmail.com>
2025-03-01 22:26:24 -0600

    Started work on byod script

---
+++ js/byod.js
@@ -0,0 +1,6 @@
+function registerBYOD() {
+    const domain = document.getElementById("domain-input").value;
+    console.log(domain);
+}
+
+document.getElementById("submit-button").addEventListener("click", registerBYOD);
