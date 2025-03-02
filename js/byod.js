89fa2a7f8967c2f693bf96d87f18b139eaafbbe4
BenRogozinski <brogodev@gmail.com>
2025-03-01 23:48:34 -0600

    Changed BYOD link to local server

--- js/byod.js
+++ js/byod.js
@@ -1,7 +1,7 @@
 async function registerBYOD(event) {
     const domain = document.getElementById("domain-input").value;
     if (domain) {
-        registrationStatusResponse = await fetch("https://dev.benrogo.net/edge-api/registerBYOD", {
+        registrationStatusResponse = await fetch("/edge-api/registerBYOD", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
