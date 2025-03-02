ec82fe25c856d3dccc32a8a64893c2f582c6f416
BenRogozinski <brogodev@gmail.com>
2025-03-01 22:32:25 -0600

    Changed form submission logic

--- js/byod.js
+++ js/byod.js
@@ -1,6 +1,7 @@
-function registerBYOD() {
+function registerBYOD(event) {
+    event.preventDefault();
     const domain = document.getElementById("domain-input").value;
     console.log(domain);
 }
 
-document.getElementById("submit-button").addEventListener("click", registerBYOD);
+document.getElementById("submit-button").addEventListener("submit", registerBYOD);
